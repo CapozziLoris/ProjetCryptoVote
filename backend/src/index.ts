b1f0b5fc78b4cdf882d8a00e1dc0ce1323ed6150
CapozziLoris <114977743+CapozziLoris@users.noreply.github.com>
2025-01-06 10:23:22 +0000

    DebutBackend- Express.js - Prisma - SQlite

---
+++ backend/src/index.ts
@@ -0,0 +1,77 @@
+// Title : Index.ts
+// Description : Server module for voting system
+// Author: Loris Capozzi
+// Version 0.1
+
+//Import des dépendances requises
+//import { PrismaClient } from "@prisma/client";
+import express, { Application, Request, Response, NextFunction } from "express";
+import { existsSync, mkdirSync, writeFileSync, createWriteStream } from "fs";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import path from "path";
+
+//Chemin de destination du fichier de Logs
+const logDirectory = path.join(__dirname, "..", "logs");
+const logFilePath = path.join(logDirectory, "server.log");
+
+//Vérifie si le répertoire de logs existe, sinon le créer
+if (!existsSync(logDirectory)) {
+  mkdirSync(logDirectory);
+}
+//Vérifie si le fichier de logs existe, sinon le créer
+if (!existsSync(logFilePath)) {
+  writeFileSync(logFilePath, "");
+}
+//Flux d'écriture pour le fichier de logs
+const logStream = createWriteStream(logFilePath, { flags: "a" });
+
+//Charger les variables d'environnement depuis le fichier .env
+dotenv.config();
+
+//Déclaration de constantes pour ExpressJS
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const HOST = process.env.HOST || "localhost";
+const app: Application = express();
+
+
+//Middleware express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Gestion des cookies par cookie-parser
+//app.use(cookie());
+
+//Affichage et sauvegarde des logs dans la console et dans un fichier texte
+app.use(morgan("combined", { stream: logStream }));
+app.use(morgan("combined"));
+
+//Texte descriptif de l'API à sa racine
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    author: "Loris Capozzi",
+    version: "0.1",
+    description: "Server Module API for Voting System project",
+    message: "Welcome to the root of the project",
+  });
+});
+
+// Middleware pour authentifier l'utilisateur
+//app.use(authenticateUser);
+
+//Défini le fichier de routes
+//app.use("/api", authRouter);
+
+//Gestion des erreurs ExpressJS
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Error on express server" });;
+});
+
+//Le serveur écoute sur l'IP et le port défini
+app.listen(PORT, HOST, () => {
+  console.log(`Server listenning on ${HOST}:${PORT}`);
+});
+
+//Export de prisma pour la gestion de base de données
+//export const prisma = new PrismaClient();
