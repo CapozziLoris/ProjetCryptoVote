6401454ddd1537a9a2da5543312414bc70768e5b
CapozziLoris <capozzi.loris@gmail.com>
2025-01-06 11:50:32 +0000

    BackEnd- En cours (Patch)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,50 +3,62 @@
 // Author: Loris Capozzi
 // Version 0.1
 
-//Import des dépendances requises
-//import { PrismaClient } from "@prisma/client";
+// Import des dépendances requises
+import { PrismaClient } from "@prisma/client";
 import express, { Application, Request, Response, NextFunction } from "express";
+import session from 'express-session';
 import { existsSync, mkdirSync, writeFileSync, createWriteStream } from "fs";
-import morgan from "morgan";
 import dotenv from "dotenv";
+//import cookie from "cookie-parser";
+import morgan from "morgan";
 import path from "path";
 
-//Chemin de destination du fichier de Logs
+
+//import authRoutes from './routes/routes';
+
+// Chemin de destination du fichier de Logs
 const logDirectory = path.join(__dirname, "..", "logs");
 const logFilePath = path.join(logDirectory, "server.log");
 
-//Vérifie si le répertoire de logs existe, sinon le créer
+// Vérifie si le répertoire de logs existe, sinon le créer
 if (!existsSync(logDirectory)) {
   mkdirSync(logDirectory);
 }
-//Vérifie si le fichier de logs existe, sinon le créer
+// Vérifie si le fichier de logs existe, sinon le créer
 if (!existsSync(logFilePath)) {
   writeFileSync(logFilePath, "");
 }
-//Flux d'écriture pour le fichier de logs
+// Flux d'écriture pour le fichier de logs
 const logStream = createWriteStream(logFilePath, { flags: "a" });
 
-//Charger les variables d'environnement depuis le fichier .env
+// Charger les variables d'environnement depuis le fichier .env
 dotenv.config();
 
-//Déclaration de constantes pour ExpressJS
+// Déclaration de constantes pour ExpressJS
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const HOST = process.env.HOST || "localhost";
 const app: Application = express();
 
+// Configuration de la session
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'MangeMonCookie',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }  // Mettre à true en production avec HTTPS
+}));
 
-//Middleware express
+// Middleware express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Gestion des cookies par cookie-parser
-//app.use(cookie());
+// Gestion des cookies par cookie-parser
+// app.use(cookie());
 
-//Affichage et sauvegarde des logs dans la console et dans un fichier texte
+// Affichage et sauvegarde des logs dans la console et dans un fichier texte
 app.use(morgan("combined", { stream: logStream }));
 app.use(morgan("combined"));
 
-//Texte descriptif de l'API à sa racine
+// Texte descriptif de l'API à sa racine
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     author: "Loris Capozzi",
@@ -59,19 +71,19 @@ app.get("/", (req: Request, res: Response) => {
 // Middleware pour authentifier l'utilisateur
 //app.use(authenticateUser);
 
-//Défini le fichier de routes
-//app.use("/api", authRouter);
+// Défini le fichier de routes
+//app.use("/api", authRoutes);
 
-//Gestion des erreurs ExpressJS
+// Gestion des erreurs ExpressJS
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).json({ error: "Error on express server" });;
 });
 
-//Le serveur écoute sur l'IP et le port défini
+// Le serveur écoute sur l'IP et le port défini
 app.listen(PORT, HOST, () => {
   console.log(`Server listenning on ${HOST}:${PORT}`);
 });
 
-//Export de prisma pour la gestion de base de données
-//export const prisma = new PrismaClient();
+// Export de prisma pour la gestion de base de données
+export const prisma = new PrismaClient();
---
+++ backend/src/routes/routes.ts
@@ -0,0 +1,25 @@
+//Titre: Auth Routes
+//Description : Routes module for voting system
+//Author: Loris Capozzi
+//Version 0.1
+import express from "express";
+//import authCreateAccount from "../controllers/auth.CreateAccount";
+//import authUserLogin from "../controllers/auth.UserLogin";
+//import authUserLogout from "../controllers/auth.UserLogout";
+const router = express.Router();
+
+// Routes pour les requêtes de l'API REST
+
+// Authentification
+//router.post("/auth/createAccount", authCreateAccount.createUserAccount);
+//router.post("/auth/login", authUserLogin.userLogin);
+//router.post("/auth/logout", authUserLogout.userLogout);
+
+// Vote
+//router.post("/voting_system/vote", );
+
+// Gestion des comptes
+//router.patch("/p9pm_admin/edit_account", adminUsers.EditUserAccount);
+//router.delete("/p9pm_admin/delete_account", adminUsers.DeleteUserAccount);
+
+export default router;
